354052793545babfa23be10ea590d96ef8f52dba
TRINADH1! <reddy.trinadhm50@gmail.com>
2015-05-28 17:27:11 +0530

    completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-app.js
+
+Calci = {
+
+  clearPreview: function() {
+    $('#preview').html("");
+    $('#result').html("0");    
+  },
+
+  deleteCharFromPreview: function() {
+    var preview = $('#preview').html();
+    $('#preview').html(preview.slice(0, preview.length-1));
+  },
+  calculateResult: function() {
+    $('#result').html(eval($('#preview').html()));
+  },
+  handleKeyPress: function(key) {
+    switch($(key).text()) {
+    case "AC":
+      Calci.clearPreview();
+      break;
+    case "DEL":
+      Calci.deleteCharFromPreview();
+      break;
+    case "=":
+      Calci.calculateResult();
+      break;
+    default:
+      $('#preview').html(
+        $('#preview').html() + $(key).text()
+      );
+    }
+  }
+};
+
+$(document).ready(function() {
+  $('.key').click(function(event){
+   Calci.handleKeyPress(this);
+  });
+});
